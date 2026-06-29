04a8e5634e33ade488b058e6e981a5f79b9a9317
uzueer <syeduzairsnu@gmail.com>
2026-06-29 13:50:24 +0530

    express setup

---
+++ backend/src/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Flow Sheriff API is running 🚀",
+  });
+});
+
+module.exports = app;
